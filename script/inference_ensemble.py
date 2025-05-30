512d2774894d3072e7cfe1003084e0bd67bc35aa
eogus <kddhh2021@gmail.com>
2025-05-30 21:28:26 +0900

    ensemble 3 models

--- script/inference_ensemble.py
+++ script/inference_ensemble.py
@@ -1,14 +1,35 @@
-# inference_ensemble.py
-
 import os
 import pandas as pd
 import torch
+import torch.nn as nn
+import timm
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from sklearn import preprocessing
 from my_config import config
 from my_dataset import load_dataframe, StoneDataset
 from my_model import get_model
+from tqdm import tqdm
+
+class ConvNeXtV2WithDropout(nn.Module):
+    def __init__(self, num_classes=7, dropout_p=0.3):
+        super().__init__()
+        self.model = timm.create_model("convnextv2_tiny", pretrained=False)
+        in_features = self.model.head.fc.in_features
+        self.model.head.fc = nn.Sequential(
+            nn.Linear(in_features, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        return self.model(x)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"[Ensemble Inference Mode] Using device: {device}")
@@ -31,37 +52,49 @@ test_loader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=Fal
 le = preprocessing.LabelEncoder()
 le.fit(train_df['label'])
 
-model1 = 'resnet101'
-model2 = 'xception65'
-model1_path = 'path'
-model2_path = 'path'
+model1_path = r"D:\dh\Dacon\Xception\resnet101_model_epoch_50.pth"
+model2_path = r"D:\dh\Dacon\Xception\output\best_model.pth"
+model3_path = r"D:\dh\Dacon\Xception\convnextv2_best_model.pth"
+
+model1_name = 'resnet101'
+model2_name = 'xception65'
+model3_name = 'convnextv2_tiny'
 
 # 모델 1: resnet101
-model1 = get_model(model1, num_classes=config.num_classes, pretrained=False)
-model1.load_state_dict(torch.load(model1_path, map_location=device))
-model1 = model1.to(device)
-model1.eval()
+resnet_model = get_model(model1_name, num_classes=config.num_classes, pretrained=False)
+resnet_model.load_state_dict(torch.load(model1_path, map_location=device))
+resnet_model = resnet_model.to(device)
+resnet_model.eval()
 
 # 모델 2: xception65
-model2 = get_model(model2, num_classes=config.num_classes, pretrained=False)
-model2.load_state_dict(torch.load(model2_path, map_location=device))
-model2 = model2.to(device)
-model2.eval()
+xception_model = get_model(model2_name, num_classes=config.num_classes, pretrained=False)
+xception_model.load_state_dict(torch.load(model2_path, map_location=device))
+xception_model = xception_model.to(device)
+xception_model.eval()
+
+# 모델 3: convnextv2
+convnext_model = ConvNeXtV2WithDropout(num_classes=config.num_classes, dropout_p=0.3)
+convnext_model.load_state_dict(torch.load(model3_path, map_location=device))
+convnext_model = convnext_model.to(device)
+convnext_model.eval()
 
 # 🔍 추론 및 앙상블 (soft voting)
 all_preds = []
 
 with torch.no_grad():
-    for images, _ in test_loader:
+    for images in tqdm(test_loader, desc="🔍 Inference", ncols=100):
         images = images.to(device)
 
-        out1 = model1(images)
-        out2 = model2(images)
+        out1 = resnet_model(images)
+        out2 = xception_model(images)
+        out3 = convnext_model(images)
 
         prob1 = F.softmax(out1, dim=1)
         prob2 = F.softmax(out2, dim=1)
+        prob3 = F.softmax(out3, dim=1)
 
-        ensemble_prob = (prob1 + prob2) / 2
+        # 3개의 모델의 확률을 평균하여 앙상블
+        ensemble_prob = (prob1 + prob2 + prob3) / 3
         preds = ensemble_prob.argmax(dim=1)
 
         # 클래스 인덱스를 라벨명으로 변환
